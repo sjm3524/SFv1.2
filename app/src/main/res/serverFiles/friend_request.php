180ced0a827b02615bbc3d0c57d9fb0c3399047c
sjm3524 <sjm3524@g.rit.edu>
2018-05-23 22:46:25 -0400

    fixed friend_request.php

--- app/src/main/res/serverFiles/friend_request.php
+++ app/src/main/res/serverFiles/friend_request.php
@@ -1,7 +1,5 @@
 <?php
-//include 'config.php';
 
-//echo 'response';
 $con = mysqli_connect("localhost", "dbadmin01", "W0rldView!!", "dev-space-db06");
 
 mysqli_set_charset($con, 'utf8');
@@ -9,39 +7,34 @@ mysqli_set_charset($con, 'utf8');
 $userID = $_POST["user"];
 $friendID = $_POST["friend"];
 
-//echo $userID;
-//echo $friendID;
 
-//$response = array();
+
 $statement = mysqli_prepare($con, "INSERT INTO friends (user, friend) VALUES (?, ?)");
-//$response["success"] = false;
-
-//$statement = mysqli_prepare($con, "INSERT INTO user (name, username, email, password) VALUES ($name, $username, $email, $password)";
 
 mysqli_stmt_bind_param($statement, "ss", $userID, $friendID);
 mysqli_stmt_execute($statement);
 
-$check_verified = mysqli_prepare($con, "SELECT * FROM friends WHERE friend = $userID");
+
+$verified =1;
+
+//$scon = mysqli_connect("localhost", "dbadmin01", "W0rldView!!", "dev-space-db06");
+
+$check_stmt ="UPDATE friends SET verified = '" . $verified . "' where friend = '". $userID ."' and user = '". $friendID ."'";
+$verify = mysqli_query($con, $check_stmt);
+
+
+if (mysqli_affected_rows($con)>0) { 
+	
+	$query = mysqli_query($con ,"UPDATE friends SET verified= '" . $verified. "' WHERE friend='". $friendID ."' AND user = '". $userID ."'");
+	
+	
+	}
 
 
 
-mysqli_stmt_execute($check_verified);
 
-//mysqli_stmt_store_result($check_verified);
-//mysqli_stmt_bind_result($check_verified, $row, $user, $friend, $verified);
 
-$verified=1;
 
-$sql = mysql_query("SELECT user FROM friends WHERE friend = '".$userID."'");
-            if (mysql_num_rows($sql) != 0) {
-				while(mysqli_stmt_fetch($statement)){
-					if (
-					
-					echo json_encode($response);
-				}
-			}
-$query = mysqli_prepare($con ,"UPDATE friends SET verified= ".$verified." WHERE friend='".$userID."' AND user = '".$userID."'");
-mysqli_stmt_execute($query);
 
 
 
@@ -50,5 +43,8 @@ mysqli_stmt_execute($query);
 $response= "true";
 
 echo $response;
+//echo "hi";
+//echo $userID;
+//echo $friendID;
 
 ?>
